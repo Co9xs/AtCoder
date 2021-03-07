48aeaf42a67671bd9780f882a2c2bdc5a65e04f3
Ryo Fujishima <57798045+Co9xs@users.noreply.github.com>
2021-03-07 18:22:49 +0900

    ABC085B - Kagami Mochi

---
+++ AtCoderBeginnersSelection/KagamiMochi.py
@@ -0,0 +1,6 @@
+ import numpy as np
+N = int(input())
+mochi_list = sorted([int(input()) for n in range(N)])
+diff_list = np.diff(mochi_list)
+can_stack_num = len([1 for diff in diff_list if diff>0]) + 1
+print(can_stack_num)
