9d3593c842f3ed61a7653a5e010328eb2c260e96
Ryo Fujishima <57798045+Co9xs@users.noreply.github.com>
2021-03-22 14:47:44 +0900

    Create To3.py

---
+++ To3.py
@@ -0,0 +1,21 @@
+N = input()
+D = len(N)
+ans = len(N)
+if int(N)%3 == 0:
+  print(0)
+  exit()
+
+for i in range(2**D):
+  num = 0
+  cnt = 0
+  for j in range(D):
+    if (i>>j) & 1:
+      num += int(N[j]) #1が立っていたらnumに残す数を記録していく
+      cnt += 1 #何桁残すか
+    if num % 3 == 0:
+      ans = min(ans, D-cnt)#D-cnt(何桁消したか)で答えを更新していく
+
+if ans == D:
+  print(-1)
+else:
+  print(ans)
