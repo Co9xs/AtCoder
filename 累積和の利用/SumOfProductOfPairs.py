0ce9608732fa76254c12afeb92d79ee3d2b2d175
Ryo Fujishima <57798045+Co9xs@users.noreply.github.com>
2021-03-25 15:38:08 +0900

    Create SumOfProductOfPairs.py

---
+++ 累積和の利用/SumOfProductOfPairs.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = [int(x) for x in input().split()]
+mod = 10**9+7
+a_cum = [0] * (n+1)
+# 累積和の計算
+for i in range(n):
+    a_cum[i + 1] = a_cum[i] + A[i]
+ans = 0
+for i in range(len(A)-1):
+  ans += A[i] * (a_cum[n] - a_cum[i+1])
+ans %= mod
+print(ans)
